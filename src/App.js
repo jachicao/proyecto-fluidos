ceddb4dc506f051ae29644c75d0225a8dd8c19a3
jachicao <jachicao@uc.cl>
2016-10-02 03:42:11 -0300

    Save local changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { Navbar, Nav, NavItem, MenuItem, NavDropdown, Alert, Button } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, MenuItem, NavDropdown, Alert, Button, Jumbotron } from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -38,6 +38,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        {/*
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
           </p>
@@ -45,6 +46,12 @@ class App extends Component {
           <strong>It works!</strong> Good job :)!.
         </Alert>
         <Button bsStyle="info" bsSize="large">Info button</Button>
+        */}
+        <Jumbotron>
+            <h1>Hello, world!</h1>
+            <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <p><Button bsStyle="primary">Learn more</Button></p>
+        </Jumbotron>
       </div>
     );
   }
