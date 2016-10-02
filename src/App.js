c99b64eb676ed0badb71ba9cb2386314f8e4c90a
jachicao <jachicao@uc.cl>
2016-10-02 03:34:44 -0300

    Save local changes

--- src/App.js
+++ src/App.js
@@ -9,20 +9,30 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Hola mundo</a>
-            </Navbar.Brand>
-          </Navbar.Header>
+      <Navbar inverse>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">React-Bootstrap</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
           <Nav>
             <NavItem eventKey={1} href="#">Link</NavItem>
-            <NavItem eventKey={2} href="#">Link2</NavItem>
+            <NavItem eventKey={2} href="#">Link</NavItem>
             <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
               <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Action2</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
             </NavDropdown>
           </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">Link Right</NavItem>
+            <NavItem eventKey={2} href="#">Link Right</NavItem>
+          </Nav>
+        </Navbar.Collapse>
         </Navbar>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -34,7 +44,7 @@ class App extends Component {
         <Alert bsStyle='success'>
           <strong>It works!</strong> Good job :)!.
         </Alert>
-        <Button bsStyle="info" bsSize="large">Boton bkn</Button>
+        <Button bsStyle="info" bsSize="large">Info button</Button>
       </div>
     );
   }
